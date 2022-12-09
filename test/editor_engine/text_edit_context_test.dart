680931ba1d30717e7e7c669d8c79597034feb597
Yiannis Charalambous <yiannis128@hotmail.com>
2022-12-09 12:03:46 +0000

    TextEditContext Tests:
    - Begin writing tests

--- test/editor_engine/text_edit_context_test.dart
+++ test/editor_engine/text_edit_context_test.dart
@@ -1,7 +1,47 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:markdown_editor/src/editor_engine/text_edit_context.dart';
+
+import '../test_case.dart';
+
+const String _testText1 = """AAAAABBBBB
+AAAAABBBBB""";
 
 void main() {
   group("Test TextEditContext.apply", () {
+    var tests = <TestCase>[
+      TestCase(
+          title: "Replace text once",
+          test: TextEditContext.fromArray(
+            const TextSelection(
+              baseOffset: 11,
+              extentOffset: 16,
+            ),
+            [
+              TextEdit("CCCCC", 11, 16, 0, 0),
+            ],
+          ),
+          expected: "AAAAABBBBB\nCCCCCBBBBB"),
+    ];
 
+    // TODO Write more TextEditContext Tests
+    // Replace text once with selection offset increase
+    // Replace text once with selection offset decrease
+    // Replace text multiple with selection offset increase
+    // Replace text multiple with selection offset decrease
+    // Test reverse parameter
+
+    for (var i = 0; i < tests.length; i++) {
+      var t = tests[i];
+      var title = t.title;
+      TextEditContext ctx = t.test;
+      var expected = t.expected;
+
+      test(title, () {
+        var result = ctx.apply(_testText1);
+
+        expect(result, expected);
+      });
+    }
   });
 }
